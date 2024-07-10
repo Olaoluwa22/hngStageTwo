df5484e3af5ade00bc2ff0827eb46f8b5419e4bf
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-07-10 06:17:36 +0100

    Renamed a controller

--- src/main/java/com/hng/stagetwo/controller/OrganizationController.java
+++ src/main/java/com/hng/stagetwo/controller/OrganizationController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/organizations")
+@RequestMapping("/api/organisations")
 public class OrganizationController {
     @Autowired
     private OrganizationService organizationService;

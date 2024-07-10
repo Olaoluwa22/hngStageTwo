57115d458e37e616bd56e0e845277d6db80721da
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-07-10 09:58:09 +0100

    Changed Organization to Organisation

--- src/main/java/com/hng/stagetwo/controller/OrganisationController.java
+++ src/main/java/com/hng/stagetwo/controller/OrganisationController.java
@@ -14,19 +14,19 @@ public class OrganisationController {
     @Autowired
     private OrganizationService organizationService;
     @GetMapping
-    public ResponseEntity<?> getUserOrganizations(){
+    public ResponseEntity<?> getUserOrganisations(){
         return organizationService.getUserOrganizations();
     }
     @GetMapping("/{orgId}")
-    public ResponseEntity<?> getOrganization(@PathVariable String orgId){
+    public ResponseEntity<?> getOrganisation(@PathVariable String orgId){
         return organizationService.getOrganization(orgId);
     }
     @PostMapping
-    public ResponseEntity<?> createOrganization(@Valid @RequestBody OrganizationRequestDto requestDto){
+    public ResponseEntity<?> createOrganisation(@Valid @RequestBody OrganizationRequestDto requestDto){
         return organizationService.createOrganization(requestDto);
     }
     @PostMapping("/{orgId}/users")
-    public ResponseEntity<?> addUserToOrganization(@Valid @RequestBody AddUserRequest request, @PathVariable String orgId){
+    public ResponseEntity<?> addUserToOrganisation(@Valid @RequestBody AddUserRequest request, @PathVariable String orgId){
         return organizationService.addUserToOrganization(orgId, request);
     }
 }
